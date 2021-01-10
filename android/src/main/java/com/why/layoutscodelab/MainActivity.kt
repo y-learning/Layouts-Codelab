8866afcdd93658754a42b6973d0a0ec63a8c2b92
Yahya <yahyatinani@gmail.com>
2021-01-10 13:18:13 +0100

    Create a padding modifier to make it relative to the baseline

--- android/src/main/java/com/why/layoutscodelab/MainActivity.kt
+++ android/src/main/java/com/why/layoutscodelab/MainActivity.kt
@@ -33,11 +33,35 @@ import androidx.compose.runtime.Providers
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.platform.setContent
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.why.layoutscodelab.themes.LayoutsCodelabTheme
+
+fun Modifier.firstBaselineToTop(firstBaselineToTop: Dp) =
+    Modifier.layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Calculate y position for placeable, where firstBaseline is
+        // not a position relative to 0.
+        val placeableY = firstBaselineToTop.toIntPx() - firstBaseline
+
+        // the height of the placeable is the position y + the placeable height
+        val height = placeableY + placeable.height
+
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
 
 @Composable
 fun Avatar(modifier: Modifier = Modifier) {
@@ -183,6 +207,22 @@ fun LayoutsCodelabPreview() {
 fun PhotographerCardPreview() {
     LayoutsCodelabTheme {
         PhotographerCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsCodelabTheme {
+        Text(text = "Hi There!", modifier = Modifier.firstBaselineToTop(24.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutsCodelabTheme {
+        Text("Hi there!", Modifier.padding(top = 24.dp))
     }
 }
 
